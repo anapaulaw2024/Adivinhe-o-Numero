ee6cba7a837cb989d6d37442e30886481e5e2c21
AnaWulCard <156938112+anapaulaw2024@users.noreply.github.com>
2024-10-07 12:56:53 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+alert('Bem-vind@s ao jogo Adivinhe o Número!')
+let numeroSecreto= parseInt(Math.random() * numeroMaximo + 1)
+let chute;
+let tentativas = 1;
+let numeroMaximo = 100
+
+while (chute != numeroSecreto) {
+chute = prompt(`Digite um número entre 1 e ${numeroMaximo}`);
+    if (chute == numeroSecreto ){
+        break;
+        
+    }else{
+    
+}
+    if(chute > numeroSecreto){
+        alert(`O número secreto é menor que ${chute}`);
+}
+    else{
+        alert(`O número secreto é maior que ${chute}`);
+    }
+    ///tentativas = tentativas + 1;
+    tentativas ++
+    
+}
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+alert(`Você acertou o número${numeroSecreto}com${tentativas}${palavraTentativa}`);
